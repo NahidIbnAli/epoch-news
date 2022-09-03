1dfedb4b8445e7b9b464f8dd83383b36c9afe206
Nahid Ibn Ali <nahidibnali7@gmail.com>
2022-09-04 02:54:42 +0600

    added sorting function to display most view news on the top

--- js/app.js
+++ js/app.js
@@ -1,11 +1,17 @@
-const loadData = () => {
-    fetch('https://openapi.programming-hero.com/api/news/categories')
-    .then(res => res.json())
-    .then(data => displayNewsCategories(data.data.news_category))
-    .catch(error => console.log(error));
+// Function for calling api to load category info 
+const loadData = async () => {
+  try {
+    const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await res.json();
+    displayNewsCategories(data.data.news_category);
+  }
+  catch(error) {
+    console.log(error);
+  }    
 }
 
-const displayNewsCategories = (newsCategories) => {
+// Function for display category name on ui
+const displayNewsCategories = newsCategories => {
     const categoryContainer = document.getElementById('category-container');
     newsCategories.forEach(category => {
         const li = document.createElement('li');
@@ -16,27 +22,36 @@ const displayNewsCategories = (newsCategories) => {
     })
 }
 
-const loadNews = categoryId => {
+// Function for calling api using category id dynamically
+const loadNews = async categoryId => {
     // Start loader
     toggleSpinner(true);
-    fetch(`https://openapi.programming-hero.com/api/news/category/0${categoryId}`)
-    .then(res => res.json())
-    .then(data => displayAllNewsInACategory(data.data))
-    .catch(error => console.log(error));
+    try {
+      const res = await fetch(`https://openapi.programming-hero.com/api/news/category/0${categoryId}`);
+      const data = await res.json();
+      const arrayOfObject = data.data;
+      arrayOfObject.sort((a, b) => b.total_view - a.total_view);
+      displayNews(arrayOfObject);
+    }
+    catch(error) {
+      console.log(error);
+    }
+    
 }
 
-const displayAllNewsInACategory = allNews => {
+// Function for display all news of a category on a section dynamically
+const displayNews = news => {
     const noDataFoundField = document.getElementById('no-data-found-field');
-    if(allNews.length === 0) {
+    if(news.length === 0) {
         noDataFoundField.classList.remove('d-none')
     }
     else {
         noDataFoundField.classList.add('d-none')
     }
-    document.getElementById('found-item').innerText = `${allNews.length} items found for this category`
+    document.getElementById('found-item').innerText = `${news.length} items found for this category`
     const newsContainer = document.getElementById('news-container');
     newsContainer.textContent = '';
-    allNews.forEach(news => {
+    news.forEach(news => {
         const div = document.createElement('div');
         div.classList.add('col-12');
         div.innerHTML = `
@@ -47,8 +62,8 @@ const displayAllNewsInACategory = allNews => {
                 </div>
                 <div class="col-md-9">
                   <div class="card-body ps-md-5">
-                    <h5 class="card-title fw-bold">${news.title}</h5>
-                    <p class="card-text text-muted text-truncate">${news.details}</p>
+                    <h4 class="card-title fw-bold">${news.title}</h4>
+                    <p class="card-text text-muted">${news.details.slice(0, 700)}...</p>
                     <div
                       class="d-flex flex-column flex-md-row gap-2 justify-content-between align-items-center"
                     >
@@ -85,42 +100,50 @@ const displayAllNewsInACategory = allNews => {
     toggleSpinner(false);
 }
 
+// Function for calling api by using news id dynamically
 const loadDetail = async newsId => {
-    const res = await fetch(`https://openapi.programming-hero.com/api/news/${newsId}`);
-    const data = await res.json();
-    displayNewsDetail(data.data[0])
+    try {
+      const res = await fetch(`https://openapi.programming-hero.com/api/news/${newsId}`);
+      const data = await res.json();
+      displayNewsDetail(data.data[0]);
+    }
+    catch(error) {
+      console.log(error);
+    }
+    
 }
 
-const displayNewsDetail = details => {
-    console.log(details);
+// Function for display news detail info on the modal dynamically
+const displayNewsDetail = newsDetails => {
     const modalContainer = document.getElementById('modal-container');
     modalContainer.textContent = '';
     const div = document.createElement('div');
     div.innerHTML = `
-    <img src="${details.image_url}" class="img-fluid w-100 rounded-start" alt="..." />
-    <h4 class="my-2">${details.title}</h4>
+    <img src="${newsDetails.image_url}" class="img-fluid w-100 rounded-start" alt="..." />
+    <h4 class="my-2">${newsDetails.title}</h4>
     <div class="d-flex align-items-center gap-3 my-3">
           <div>
-            <img src="${details.author.img}" width="50px" class="rounded-circle" alt="" />
+            <img src="${newsDetails.author.img}" width="50px" class="rounded-circle" alt="" />
           </div>
           <div>
-            <p class="m-0 fw-semibold">${details.author.name ? details.author.name : 'No data found'}</p>
-            <small class="text-muted">${details.author.published_date ? details.author.published_date : 'No data found'}</small>
+            <p class="m-0 fw-semibold">${newsDetails.author.name ? newsDetails.author.name : 'No data found'}</p>
+            <small class="text-muted">${newsDetails.author.published_date ? newsDetails.author.published_date : 'No data found'}</small>
           </div>
     </div>
     <div class="my-4">
-        <p>${details.details}</p>
+        <p>${newsDetails.details}</p>
     </div>
     <div>
         <p class="text-center">
             <i class="fa-regular fa-eye"></i>
-            <span class="fw-semibold">${details.total_view ? details.total_view : 'No data found'}</span>
+            <span class="fw-semibold">${newsDetails.total_view ? newsDetails.total_view : 'No data found'}</span>
         </p>
     </div>
     `
     modalContainer.appendChild(div);
 }
 
+// Function for loading spinner
 const toggleSpinner = isLoading => {
   const loaderSection = document.getElementById('loader-section');
   if(isLoading) {
@@ -130,5 +153,7 @@ const toggleSpinner = isLoading => {
       loaderSection.classList.add('d-none');
   }
 }
+
+loadNews(1);
 
 loadData();
